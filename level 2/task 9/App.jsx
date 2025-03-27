a46f3b9d4ce060ce11ada79ac718c17620fd0655
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-27 10:25:25 +0530

    Create App.jsx

---
+++ level 2/task 9/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function DynamicInput() {
+  const [text, setText] = useState("");
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Type something..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="input-box"
+      />
+      <p className="display-text">{text}</p>
+    </div>
+  );
+}
