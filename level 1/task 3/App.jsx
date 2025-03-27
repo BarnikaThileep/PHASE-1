88ef0543c9dde6cbf13d08abf2d0687ee0d5575f
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-27 09:16:20 +0530

    Create App.jsx

---
+++ level 1/task 3/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Hello = () => {
+return(
+  <div>
+    <h1>
+    Hello Barnika
+    </h1>
+    <p>Amiable Creative IT initiator,currently pursuing B.Tech IT in SNS College Of Engineering With GPA of 9.11</p>
+</div>
+)
+}
+export default Hello
