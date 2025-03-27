7e512a92e8f9afca6c0abb9980f5bf49ac534344
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-27 09:47:00 +0530

    Create App.jsx

---
+++ level 2/task 1/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+const HelloComponent = () => {
+  return (
+    <div>
+      <h1>Hello from a functional component!</h1>
+    </div>
+  );
+};
+
+export default HelloComponent;
