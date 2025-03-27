0faf32234938fd149e2b9249a339e9f67b5d8fe2
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-27 09:49:03 +0530

    Create App.jsx

---
+++ level 2/task 2/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React, { Component } from 'react';
+import './App.css';
+
+class HelloComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from a class component!</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloComponent;
