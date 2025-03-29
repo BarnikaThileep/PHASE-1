c7856389ed7dd1676f64067b69e9f4fb48b38a4b
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-29 11:05:49 +0530

    Create App.js

---
+++ level 11/task 9/App.js
@@ -0,0 +1,11 @@
+
+function multiply(a, b) {
+    return a * b;
+}
+const divide = function(a, b) {
+    return a / b;
+};
+const power = (base, exponent) => base ** exponent;
+console.log("Multiply (8 * 3):", multiply(8, 3));
+console.log("Divide (60 / 2):", divide(60, 2));
+console.log("Power (2^8):", power(2, 8));
