a8cc27ce402b377133894500032232f651827463
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-29 10:59:05 +0530

    Create App.js

---
+++ level 11/task 1/App.js
@@ -0,0 +1,9 @@
+
+var myVar = "Barnika";  
+let myLet = 20;         
+const myConst = "Red";  
+
+
+console.log("Name:", myVar);
+console.log("Age:", myLet);
+console.log("Favorite Color:", myConst);
