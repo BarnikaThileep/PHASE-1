673dc6d038bda74259fd24cceee35f4e1863aada
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-29 11:00:41 +0530

    Create App.js

---
+++ level 11/task 4/App.js
@@ -0,0 +1,12 @@
+
+let fullName = "Barnika"; 
+
+let upperCaseName = fullName.toUpperCase(); 
+let nameLength = fullName.length; 
+let firstName = fullName.split(" ")[0]; 
+let hometown = "Sathy"; 
+let fullInfo = fullName + " is from " + hometown + ".";
+console.log("Uppercase Name:", upperCaseName);
+console.log("Name Length:", nameLength);
+console.log("First Name:", firstName);
+console.log("Concatenated String:", fullInfo);
