cdb28bbefbe75bc642111eb558b7ed89a7f70af5
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-29 10:59:40 +0530

    Create App.js

---
+++ level 11/task 2/App.js
@@ -0,0 +1,19 @@
+
+let myString = "Hello, everyone!"; 
+let myNumber = 32;                   
+let myBoolean = true;                 
+let myNull = null;                    
+let myUndefined;                       
+let myObject = { name: "Barnika", age: 20 }; 
+
+console.log("Type of myString:", typeof myString);
+console.log("Type of myNumber:", typeof myNumber);
+console.log("Type of myBoolean:", typeof myBoolean);
+console.log("Type of myNull:", typeof myNull); 
+console.log("Type of myUndefined:", typeof myUndefined);
+console.log("Type of myObject:", typeof myObject);
+
+
+let strNumber = "100";
+let convertedNumber = parseInt(strNumber);
+console.log("Converted Number:", convertedNumber, "| Type:", typeof convertedNumber);
