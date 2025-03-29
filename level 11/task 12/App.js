27834a6b122454241c234604412abd8cbb3039be
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-29 11:07:11 +0530

    Create App.js

---
+++ level 11/task 12/App.js
@@ -0,0 +1,8 @@
+
+console.log("3x3 Multiplication Table:");
+
+for (let i = 1; i <= 3; i++) {
+    let row = ""; 
+    for (let j = 1; j <= 3; j++) {
+        row += `${i * j}\t`; 
+    console.log(row); }}
