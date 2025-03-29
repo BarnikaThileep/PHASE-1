85d3766c62d4affb0ebd5107d7848c4f9102b9ef
Barnika L T <barnikathileepkumar@gmail.com>
2025-03-29 11:01:57 +0530

    Create App.js

---
+++ level 11/task 5/App.js
@@ -0,0 +1,9 @@
+
+let age = 25; 
+if (age >= 21) {
+    console.log("You are going to job");
+} else if (age <= 18) {
+    console.log("You are going to school");
+} else {
+    console.log("You are a baby");
+}
